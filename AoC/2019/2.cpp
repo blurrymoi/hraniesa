5f37a227e965c5adb94c17f7dc283d9012e9c7e4
blurry <>
2020-07-07 15:03:41 +0200

    AoC.2019: day 2 (cpp)

---
+++ AoC/2019/2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+
+using vec_int = std::vector< int >;
+
+void parse_code( const char* filename, vec_int& v )
+{
+    std::ifstream f( filename );
+    int i;
+    char c;
+
+    while( f >> i >> c )
+    {
+        v.push_back( i );
+
+        if( c == '\n' )
+            std::cerr << "newline" << std::endl;
+        else if( c != ',' )
+            std::cerr << "unknown char: " << c << std::endl;
+    }
+
+    assert( f.eof() );
+    v.push_back( i );
+}
+
+void print( vec_int& v )
+{
+    for( int p : v )
+        std::cout << p << " ";
+    std::cout << std::endl;
+}
+
+
+vec_int::iterator at( vec_int& v, unsigned pos, int offset )
+{
+    int position = v[ pos + offset ];
+    return v.begin() + position;
+}
+
+bool perform_op( std::vector< int >& v, unsigned pos )
+{
+    int opcode = v[ pos ];
+
+    switch( opcode )
+    {
+        case 1:
+            *at( v, pos, 3 ) = *at( v, pos, 1 ) + *at( v, pos, 2 );
+            break;
+        case 2:
+            *at( v, pos, 3 ) = *at( v, pos, 1 ) * *at( v, pos, 2 );
+            break;
+
+        default:
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    std::vector< int > v;
+    parse_code( "2.txt", v );
+
+    int pos = 0;
+
+    while( perform_op( v, pos ) )
+        pos += 4;
+
+    if( v[ pos ] != 99 )
+        std::cerr << "unknown opcode: " << v[ pos ] << std::endl;
+    else
+        std::cerr << "success" << std::endl;
+
+    print( v );
+}
+
