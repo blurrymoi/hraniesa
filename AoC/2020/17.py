7291382a6e162669b34b3e71af9ca4f1d067b4fd
blurry <zuzan.baran@gmail.com>
2020-12-17 10:27:44 +0100

    AoC.2020: day 17, part 2

--- AoC/2020/17.py
+++ AoC/2020/17.py
@@ -4,13 +4,14 @@ def load_row( cubes, s, y ):
     x = 0
     for c in s:
         if c != '\n':
-            cubes[ (x,y,0) ] = c == '#'
+            cubes[ (x,y,0,0) ] = c == '#'
             x += 1
 
 def cubes_around( cubes, coords ):
-    x,y,z = coords
-    l = [ cubes[(x_,y_,z_)] for x_ in [x-1,x,x+1] for y_ in [y-1,y,y+1] for z_ in [z-1,z,z+1]
-          if (x_,y_,z_) != coords and (x_,y_,z_) in cubes ]
+    x,y,z,w = coords
+    l = [ cubes[(x_,y_,z_,w_)] for x_ in [x-1,x,x+1] for y_ in [y-1,y,y+1]
+                               for z_ in [z-1,z,z+1] for w_ in [w-1,w,w+1]
+          if (x_,y_,z_,w_) != coords and (x_,y_,z_,w_) in cubes ]
     return l.count( True )
 
 def update( num_cubes, state ):
@@ -21,18 +22,19 @@ def update( num_cubes, state ):
     return False
 
 def update_surrounding( cubes, new_cubes, coords ):
-    x,y,z = coords
+    x,y,z,w = coords
     for x_ in [ x-1,x,x+1 ]:
         for y_ in [ y-1,y,y+1 ]:
             for z_ in [ z-1,z,z+1 ]:
-                new_coords = ( x_,y_,z_ )
-                if new_coords not in new_cubes:
-                    state = new_coords in cubes and cubes[new_coords]
-                    new_cubes[ new_coords ] = update( cubes_around( cubes, new_coords ), state )
+                for w_ in [w-1,w,w+1]:
+                    new_coords = ( x_,y_,z_,w_ )
+                    if new_coords not in new_cubes:
+                        state = new_coords in cubes and cubes[new_coords]
+                        new_cubes[ new_coords ] = update( cubes_around( cubes, new_coords ), state )
 
 def load():
 
-    with open( "17_1.txt", "r", encoding="utf-8" ) as f:
+    with open( "17.txt", "r", encoding="utf-8" ) as f:
 
         cubes = {}
         y = 0
